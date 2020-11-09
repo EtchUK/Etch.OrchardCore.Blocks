3b34024bc92f030c8fcd2b076a7b20ca7556712c
Peter Keating <pete.keating@etchuk.com>
2020-11-09 19:40:07 +0000

    Bump to v0.3.3

--- Manifest.cs
+++ Manifest.cs
@@ -5,7 +5,7 @@ using OrchardCore.Modules.Manifest;
     Category = "Content Management",
     Description = "Blocks module enables content items to have a block based editor.",
     Name = "Blocks",
-    Version = "0.3.2",
+    Version = "0.3.3",
     Website = "https://etchuk.com"
 )]
 
