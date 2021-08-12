fb43b2881d9ccbeb3793082cce4370ce759e7597
Peter Keating <pete.keating@etchuk.com>
2021-08-12 14:26:22 +0100

    v1.0.1

--- Manifest.cs
+++ Manifest.cs
@@ -5,7 +5,7 @@ using OrchardCore.Modules.Manifest;
     Category = "Content Management",
     Description = "Blocks module enables content items to have a block based editor.",
     Name = "Blocks",
-    Version = "0.3.5",
+    Version = "1.0.1",
     Website = "https://etchuk.com"
 )]
 
