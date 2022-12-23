a05f580f087720aaf20b00e88606407dce5eb397
Peter Keating <pete.keating@etchuk.com>
2022-12-23 11:05:06 +0000

    Update editorjs dependencies to latest version

--- wwwroot/Scripts/styles/admin.js
+++ wwwroot/Scripts/styles/admin.js
@@ -16,7 +16,7 @@
   \*****************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://etch.orchardcore.blocks/./Assets/Editor.js/css/index.scss?");
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./Assets/Editor.js/css/index.scss?");
 
 /***/ })
 
